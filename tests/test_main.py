68ebd9bfe676782b850f502755515e1ba1d4d269
Songül Kızılay <203017802+Songul-Kizilay@users.noreply.github.com>
2026-08-11 14:38:25 +0300

    Prefix console responses (#133)

--- tests/test_main.py
+++ tests/test_main.py
@@ -50,7 +50,10 @@ class MainTests(unittest.TestCase):
         )
         self.assertEqual(
             console_output.call_args_list,
-            [((first_response.message,),), ((second_response.message,),)],
+            [
+                ((f"Hypatia: {first_response.message}",),),
+                ((f"Hypatia: {second_response.message}",),),
+            ],
         )
 
     def test_main_stops_application_when_input_is_interrupted(self) -> None:
@@ -126,4 +129,4 @@ class MainTests(unittest.TestCase):
         container.resolve.assert_called_once_with(Brain)
         self.assertEqual(user_input.call_args_list, [(("You: ",),)] * 3)
         brain.process.assert_called_once_with("Hello Hypatia")
-        console_output.assert_called_once_with(response.message)
+        console_output.assert_called_once_with(f"Hypatia: {response.message}")
